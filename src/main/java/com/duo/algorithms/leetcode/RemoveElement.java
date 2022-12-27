0573eed5b1a7012355af9c66b0758208fc7ec3b0
liujie <liujie2530@163.com>
2022-12-27 20:29:18 +0800

    first commit

---
+++ src/main/java/com/duo/algorithms/leetcode/RemoveElement.java
@@ -0,0 +1,11 @@
+package com.duo.algorithms.leetcode;
+
+/**
+ * @author liujie
+ * @date 2022-12-27 20:16
+ **/
+public class RemoveElement {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
